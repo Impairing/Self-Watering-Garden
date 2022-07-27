a9c71e17d4de9d43aa8c86992b28945eb9f7b11f
Impairing <109942147+Impairing@users.noreply.github.com>
2022-07-27 13:11:45 -0400

    Added code for photoresistor

--- code.ino
+++ code.ino
@@ -1,12 +1,16 @@
 int temperaturePin = A0; //signal from temperature sensor
 int soilPin = A1; //signal from moisture sensor
+int lightPin = A2; //signal from photoresistor
 int soilMoisture; //soil moisture
-int threshold = 0; //value to trigger pump
-int pump = 8; //pump output
-int soilGood = 10; //LED for when soil moisture good
-int soilBad = 11; //LED for whne soil moisture bad
-int tempGood = 12; //LED for when temp is good
-int tempBad = 13; // LED for when temp is bad
+int threshold = 20; //value to trigger pump
+int pump = 6; //pump output
+int lightLevel, high = 0, low = 1023;
+int soilGood = 8; //LED for when soil moisture good
+int soilBad = 9; //LED for whne soil moisture bad
+int tempGood = 10; //LED for when temp is good
+int tempBad = 11; // LED for when temp is bad
+int lightBad = 12; //LED for when light is bad
+int lightGood = 13; //LED for when light is good
 float voltage, degreesC, degreesF; //temperature values
 
 
@@ -15,11 +19,15 @@ void setup()
 {
   Serial.begin(9600);
   pinMode(soilPin, INPUT);
+  pinMode(temperaturePin, INPUT);
+  pinMode(lightPin, INPUT);
   pinMode(pump, OUTPUT);
   pinMode(tempGood, OUTPUT);
   pinMode(tempBad, OUTPUT);
   pinMode(soilGood, OUTPUT);
   pinMode(soilBad, OUTPUT);
+  pinMode(lightGood, OUTPUT);
+  pinMode(lightBad, OUTPUT);
   
 }
 
@@ -28,21 +36,31 @@ void loop()
 {
   //calculations
   voltage = getVoltage(temperaturePin);
-
   degreesC = (voltage - 0.5) * 100.0;
-
   degreesF = degreesC * (9.0/5.0) + 32.0;
+  
   soilMoisture = analogRead(soilPin); //capture soil moisture
-  soilMoisture = map(soilMoisture, 550, 0, 0, 100); //maps to % value
+  soilMoisture = map(soilMoisture, 1017, 0, 0, 100); //maps to % value
+
+  lightLevel = analogRead(lightPin); //capture light level
+  tune();
 
   Serial.println("voltage: " + String(voltage) + " deg C: " + String(degreesC) + " deg F: " + String(degreesF));
   Serial.println("Moisture: " + String(soilMoisture) + "%");
+  Serial.println("Light level: " + String(lightLevel));
   
-  if (degreesF > 90) {
+  if (degreesF > 65) {
     updateLED(tempGood, tempBad);
   }
-  if (degreesF < 90) {
+  else {
     updateLED(tempBad, tempGood);
+  }
+
+  if (lightLevel > 128) {
+    updateLED(lightGood, lightBad);
+  }
+  else {
+    updateLED(lightBad, lightGood);
   }
   
   if (soilMoisture > threshold) {
@@ -53,7 +71,7 @@ void loop()
     digitalWrite(pump, HIGH); //pump on
     updateLED(soilBad, soilGood);
     Serial.println("Pump on"); 
-    delay(3000); //pump runs for 1sec
+    delay(1000); //pump runs for 1 sec
     digitalWrite(pump, LOW); //pump off
     Serial.println("Pump off");
   }
@@ -70,4 +88,18 @@ void updateLED(char ledOn, char ledOff)
 float getVoltage(int pin)
 {
   return (analogRead(pin) * 0.004882814);
+}
+
+void tune()
+{
+  if (lightLevel < low)
+  {
+    low = lightLevel;
+  }
+  if (lightLevel > high)
+  {
+    high = lightLevel;
+  }
+  lightLevel = map(lightLevel, low+30, high-30, 0, 255);
+  lightLevel = constrain(lightLevel, 0, 255);
 }
